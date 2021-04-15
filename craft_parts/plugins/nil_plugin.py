6f45c94ee482b5e255ac677358e6b3c46c263a61
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-04-15 17:55:22 -0300

    plugins: add the nil plugin
    The nil plugin is used to handle parts with no source.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ craft_parts/plugins/nil_plugin.py
@@ -0,0 +1,62 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2020 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The nil plugin.
+
+Using this, parts can be defined purely by utilizing properties that are
+automatically included, e.g. stage-packages.
+"""
+
+from typing import Any, Dict, List, Set
+
+from .base import Plugin
+from .properties import PluginProperties
+
+
+class NilPluginProperties(PluginProperties):
+    """The part properties used by the nil plugin."""
+
+    @classmethod
+    def unmarshal(cls, data: Dict[str, Any]) -> "NilPluginProperties":
+        """Populate class attributes from the part specification.
+
+        :param data: A dictionary containing part properties.
+
+        :return: The populated plugin properties data object.
+        """
+        return cls()
+
+
+class NilPlugin(Plugin):
+    """The nil plugin, useful for parts with no source."""
+
+    properties_class = NilPluginProperties
+
+    def get_build_snaps(self) -> Set[str]:
+        """Return a set of required snaps to install in the build environment."""
+        return set()
+
+    def get_build_packages(self) -> Set[str]:
+        """Return a set of required packages to install in the build environment."""
+        return set()
+
+    def get_build_environment(self) -> Dict[str, str]:
+        """Return a dictionary with the environment to use in the build step."""
+        return dict()
+
+    def get_build_commands(self) -> List[str]:
+        """Return a list of commands to run during the build step."""
+        return list()
