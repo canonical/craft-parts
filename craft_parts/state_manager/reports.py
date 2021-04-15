ca4875815b5a5391c8d245fbf2973e725c8d28f8
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-04-15 15:06:40 -0300

    states: add dirty step verification
    A part step is considered dirty if one of its state properties
    or options have changed, or if one of its dependencies should run.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- craft_parts/state_manager/reports.py
+++ craft_parts/state_manager/reports.py
@@ -16,8 +16,13 @@
 
 """Provide a report on why a step is outdated."""
 
+from collections import namedtuple
+from typing import List
+
 from craft_parts.steps import Step
 from craft_parts.utils import formatting_utils
+
+Dependency = namedtuple("Dependency", ["part_name", "step"])
 
 
 class OutdatedReport:
@@ -53,6 +58,76 @@ class OutdatedReport:
 
         if self.source_modified:
             reasons.append("source")
+
+        if not reasons:
+            return ""
+
+        return "{} changed".format(formatting_utils.humanize_list(reasons, "and", "{}"))
+
+
+class DirtyReport:
+    """The DirtyReport class explains why a given step is dirty.
+
+    A dirty step is defined to be a step that has run, but since doing so one
+    of the following things have happened:
+
+    - One or more properties used by the step have changed.
+    - One of more project options have changed.
+    - One of more of its dependencies have been re-staged.
+    """
+
+    def __init__(
+        self,
+        *,
+        dirty_properties: List[str] = None,
+        dirty_project_options: List[str] = None,
+        changed_dependencies: List[Dependency] = None,
+    ) -> None:
+        """Create a new DirtyReport.
+
+        :param dirty_properties: Properties that have changed.
+        :param dirty_project_options: Project options that have changed.
+        :param changed_dependencies: Dependencies that have changed.
+        """
+        self.dirty_properties = dirty_properties
+        self.dirty_project_options = dirty_project_options
+        self.changed_dependencies = changed_dependencies
+
+    def reason(self) -> str:
+        """Get summarized report.
+
+        :return: Short summary of why the part is dirty.
+        """
+        reasons = []
+
+        reasons_count = 0
+        if self.dirty_properties:
+            reasons_count += 1
+        if self.dirty_project_options:
+            reasons_count += 1
+        if self.changed_dependencies:
+            reasons_count += 1
+
+        if self.dirty_properties:
+            # Be specific only if this is the only reason
+            if reasons_count > 1 or len(self.dirty_properties) > 1:
+                reasons.append("properties")
+            else:
+                reasons.append(f"{self.dirty_properties[0]!r} property")
+
+        if self.dirty_project_options:
+            # Be specific only if this is the only reason
+            if reasons_count > 1 or len(self.dirty_project_options) > 1:
+                reasons.append("options")
+            else:
+                reasons.append(f"{self.dirty_project_options[0]!r} option")
+
+        if self.changed_dependencies:
+            # Be specific only if this is the only reason
+            if reasons_count > 1 or len(self.changed_dependencies) > 1:
+                reasons.append("dependencies")
+            else:
+                reasons.append(f"{self.changed_dependencies[0].part_name!r}")
 
         if not reasons:
             return ""
--- tests/unit/state_manager/test_reports.py
+++ tests/unit/state_manager/test_reports.py
@@ -17,6 +17,7 @@
 import pytest
 
 from craft_parts.state_manager import reports
+from craft_parts.state_manager.reports import Dependency
 from craft_parts.steps import Step
 
 
@@ -32,5 +33,48 @@ from craft_parts.steps import Step
 def test_outdated_report(step, source_modified, result):
     report = reports.OutdatedReport(
         previous_step_modified=step, source_modified=source_modified
+    )
+    assert report.reason() == result
+
+
+@pytest.mark.parametrize(
+    "props,opts,deps,result",
+    [
+        (None, None, None, ""),
+        (["a"], None, None, "'a' property changed"),
+        (["a", "b"], None, None, "properties changed"),
+        (None, ["c"], None, "'c' option changed"),
+        (None, ["c", "d"], None, "options changed"),
+        (["a"], ["c"], None, "options and properties changed"),
+        (None, None, [Dependency("e", Step.STAGE)], "'e' changed"),
+        (
+            None,
+            None,
+            [Dependency("e", Step.STAGE), Dependency("f", Step.STAGE)],
+            "dependencies changed",
+        ),
+        (
+            ["a"],
+            None,
+            [Dependency("e", Step.STAGE)],
+            "dependencies and properties changed",
+        ),
+        (
+            None,
+            ["b"],
+            [Dependency("e", Step.STAGE)],
+            "dependencies and options changed",
+        ),
+        (
+            ["a"],
+            ["b"],
+            [Dependency("e", Step.STAGE)],
+            "dependencies, options, and properties changed",
+        ),
+    ],
+)
+def test_dirty_report(props, opts, deps, result):
+    report = reports.DirtyReport(
+        dirty_properties=props, dirty_project_options=opts, changed_dependencies=deps
     )
     assert report.reason() == result
