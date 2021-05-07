607ec3e37ac927e11ea89274b38f78c4ebead2c9
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-05-07 09:47:48 -0300

    packages: add host platform selection
    Determine the correct repository to use based on the host ID. This
    implementation is a direct port from the corresponding function
    in Snapcraft.
    
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ tests/unit/packages/test_platform.py
@@ -0,0 +1,71 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2021 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+
+from craft_parts import errors, packages
+from craft_parts.packages import platform
+from craft_parts.packages.base import DummyRepository
+from craft_parts.packages.deb import Ubuntu
+
+_deb_distros = {"ubuntu", "debian", "elementary OS", "elementary", "neon"}
+
+
+def test_deb_based_platforms():
+    assert set(platform._DEB_BASED_PLATFORM) == _deb_distros
+
+
+def test_is_deb_based():
+    for distro in [
+        "fedora",
+        "debian",
+        "ubuntu",
+        "elementary OS",
+        "elementary",
+        "neon",
+        "opensuse",
+        "other",
+    ]:
+        assert platform.is_deb_based(distro) == (distro in _deb_distros)
+
+
+def test_is_deb_based_default(mocker):
+    for distro in [
+        "fedora",
+        "debian",
+        "ubuntu",
+        "elementary OS",
+        "elementary",
+        "neon",
+        "opensuse",
+    ]:
+        mocker.patch("craft_parts.utils.os_utils.OsRelease.id", return_value=distro)
+        assert platform.is_deb_based() == (distro in _deb_distros)
+
+
+def test_is_deb_based_error(mocker):
+    mocker.patch(
+        "craft_parts.utils.os_utils.OsRelease.id", side_effect=errors.OsReleaseIdError()
+    )
+    assert platform.is_deb_based() is False
+
+
+@pytest.mark.parametrize(
+    "distro,repo", [("ubuntu", Ubuntu), ("other", DummyRepository)]
+)
+def test_get_repository_for_platform(mocker, distro, repo):
+    mocker.patch("craft_parts.utils.os_utils.OsRelease.id", return_value=distro)
+    assert packages._get_repository_for_platform() == repo
