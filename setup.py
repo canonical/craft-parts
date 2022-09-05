b899a2d6cca049bd5a3590d4d27371c89902db9f
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2022-09-05 09:48:07 -0300

    Bump version: 1.12.1 → 1.13.0

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ extras_requires = {
 
 setup(
     name="craft-parts",
-    version="1.12.1",
+    version="1.13.0",
     description="Craft parts tooling",
     long_description=readme,
     author="Canonical Ltd.",
