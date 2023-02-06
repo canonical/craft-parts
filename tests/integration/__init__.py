2ebe52ff8cc1c33178970d82de78248d7b86eb92
Alex Lowe <alex.lowe@canonical.com>
2023-02-05 22:10:53 -0600

    insert fix

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -13,8 +13,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """Fixtures for all plugin tests to use."""
 
-import pytest
 from craft_parts.packages import deb
+import pytest
 
 
 @pytest.fixture(autouse=True)
