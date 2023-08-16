7d2e1dc29e76d477435f8c4b2d13bf2ef5b137c8
liushuyu <liushuyu011@gmail.com>
2023-08-16 13:50:09 -0600

    plugins/rust_plugin: rewrite the plugin to improve UX ...
    ... the new plugin will install rustup toolchain by default (opt-out
    possible). Three new options were added to help users with different
    needs.
    
    Signed-off-by: Zixing Liu <zixing.liu@canonical.com>

--- tests/unit/plugins/test_rust_plugin.py
+++ tests/unit/plugins/test_rust_plugin.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright 2022 Canonical Ltd.
+# Copyright 2022-2023 Canonical Ltd.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
 import pytest
 from pydantic import ValidationError
 
-from craft_parts import errors
+from craft_parts.errors import PluginEnvironmentValidationError
 from craft_parts.infos import PartInfo, ProjectInfo
 from craft_parts.parts import Part
 from craft_parts.plugins.rust_plugin import RustPlugin
@@ -31,210 +31,171 @@ def part_info(new_dir):
     )
 
 
-class TestPluginRustPlugin:
-    """Rust plugin tests."""
+def test_get_build_snaps(part_info):
+    properties = RustPlugin.properties_class.unmarshal({"source": "."})
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    assert plugin.get_build_snaps() == set()
 
-    def test_validate_environment(self, dependency_fixture, part_info):
-        cargo = dependency_fixture("cargo")
-        rustc = dependency_fixture("rustc")
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
 
-        validator = plugin.validator_class(
-            part_name="my-part",
-            env=f"PATH={str(cargo.parent)}:{str(rustc.parent)}",
-            properties=properties,
-        )
-        validator.validate_environment()
+def test_get_build_packages(part_info):
+    properties = RustPlugin.properties_class.unmarshal({"source": "."})
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    assert plugin.get_build_packages() == {
+        "curl",
+        "gcc",
+        "git",
+        "pkg-config",
+        "findutils",
+    }
 
-    @pytest.mark.parametrize(
-        "dependencies",
-        [
-            ("cargo", ["rustc"]),
-            ("rustc", ["cargo"]),
-        ],
+
+def test_get_build_environment(part_info):
+    properties = RustPlugin.properties_class.unmarshal({"source": "."})
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+
+    assert plugin.get_build_environment() == {"PATH": "${HOME}/.cargo/bin:${PATH}"}
+
+
+def test_get_build_commands_default(part_info):
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "rust-channel": "stable"}
     )
-    def test_validate_environment_missing_dependencies(
-        self, dependencies, dependency_fixture, part_info
-    ):
-        """Validate that missing dependencies raise an exception.
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = lambda: False
 
-        :param dependencies: tuple consisting of 1 missing dependency
-        and a list of valid dependencies
-        """
-        missing_dependency_name, valid_dependency_names = dependencies
-        path = "PATH="
-
-        for valid_dependencies_name in valid_dependency_names:
-            valid_dependency = dependency_fixture(valid_dependencies_name)
-            path += f":{str(valid_dependency.parent)}"
-
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-
-        validator = plugin.validator_class(
-            part_name="my-part", env=path, properties=properties
-        )
-        with pytest.raises(errors.PluginEnvironmentValidationError) as raised:
-            validator.validate_environment()
-
-        assert raised.value.reason == f"'{missing_dependency_name}' not found"
-
-    @pytest.mark.parametrize(
-        "dependencies",
-        [
-            ("cargo", ["rustc"]),
-            ("rustc", ["cargo"]),
-        ],
+    commands = plugin.get_build_commands()
+    assert (
+        plugin.get_pull_commands()[0]
+        == """curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
+sh -s -- -y --no-modify-path --profile=minimal --default-toolchain stable
+"""
     )
-    def test_validate_environment_broken_dependencies(
-        self, dependencies, dependency_fixture, part_info
-    ):
-        """Validate that broken dependencies raise an exception.
+    assert 'cargo install -f --locked --path "."' in commands[0]
 
-        :param dependencies: tuple consisting of 1 broken dependency
-        and a list of valid dependencies
-        """
-        broken_dependency_name, valid_dependency_names = dependencies
-        broken_dependency = dependency_fixture(broken_dependency_name, broken=True)
-        path = f"PATH={str(broken_dependency.parent)}"
 
-        for valid_dependencies_name in valid_dependency_names:
-            valid_dependency = dependency_fixture(valid_dependencies_name)
-            path += f":{str(valid_dependency.parent)}"
+def test_get_build_commands_no_install(part_info):
+    def _check_rustup():
+        raise RuntimeError("should not be called")
 
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "rust-channel": "none"}
+    )
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = _check_rustup
 
-        validator = plugin.validator_class(
-            part_name="my-part", env=path, properties=properties
-        )
-        with pytest.raises(errors.PluginEnvironmentValidationError) as raised:
-            validator.validate_environment()
+    commands = plugin.get_build_commands()
+    assert len(commands) == 1
+    assert plugin.get_pull_commands() == []
+    assert 'cargo install -f --locked --path "."' in commands[0]
 
-        assert (
-            raised.value.reason
-            == f"'{broken_dependency_name}' failed with error code 33"
-        )
 
-    def test_validate_environment_part_dependencies(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
+def test_get_build_commands_use_lto(part_info):
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "rust-use-global-lto": True, "rust-channel": "stable"}
+    )
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = lambda: True
 
-        validator = plugin.validator_class(
-            part_name="my-part", env="PATH=/foo", properties=properties
-        )
+    commands = plugin.get_build_commands()
+    assert len(commands) == 1
+    assert "curl" not in plugin.get_pull_commands()[0]
+    assert 'cargo install -f --locked --path "."' in commands[0]
+    assert "--config 'profile.release.lto = true'" in commands[0]
+
+
+def test_get_build_commands_multiple_crates(part_info):
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "rust-path": ["a", "b", "c"], "rust-channel": "stable"}
+    )
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = lambda: True
+
+    commands = plugin.get_build_commands()
+    assert len(commands) == 3
+    assert "curl" not in plugin.get_pull_commands()[0]
+    assert 'cargo install -f --locked --path "a"' in commands[0]
+    assert 'cargo install -f --locked --path "b"' in commands[1]
+    assert 'cargo install -f --locked --path "c"' in commands[2]
+
+
+def test_get_build_commands_multiple_features(part_info):
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "rust-features": ["ft-a", "ft-b"], "rust-channel": "stable"}
+    )
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = lambda: True
+
+    commands = plugin.get_build_commands()
+    assert len(commands) == 1
+    assert "curl" not in plugin.get_pull_commands()[0]
+    assert 'cargo install -f --locked --path "."' in commands[0]
+    assert "--features 'ft-a ft-b'" in commands[0]
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        "stable",
+        "nightly",
+        "beta",
+        "1.65",
+        "1.71.1",
+        "nightly-2022-12-01",
+        "stable-x86_64-fortanix-unknown-sgx",
+        "nightly-2023-06-14-aarch64-nintendo-switch-freestanding",
+    ],
+)
+def test_get_build_commands_different_channels(part_info, value):
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "rust-channel": value}
+    )
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = lambda: False
+    commands = plugin.get_build_commands()
+    assert len(commands) == 1
+    assert f"--default-toolchain {value}" in plugin.get_pull_commands()[0]
+
+
+@pytest.mark.parametrize(
+    "rust_path",
+    [None, "i am a string", {"i am": "a dictionary"}, ["duplicate", "duplicate"]],
+)
+def test_get_build_commands_rust_path_invalid(rust_path, part_info):
+    with pytest.raises(ValidationError):
+        RustPlugin.properties_class.unmarshal({"source": ".", "rust-path": rust_path})
+
+
+def test_error_on_conflict_config(part_info):
+    properties = RustPlugin.properties_class.unmarshal(
+        {
+            "source": ".",
+            "rust-path": ["a", "b", "c"],
+            "after": ["rust-deps"],
+            "rust-channel": "stable",
+        }
+    )
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    validator = plugin.validator_class(
+        part_name="my-part", properties=properties, env=""
+    )
+    with pytest.raises(PluginEnvironmentValidationError):
         validator.validate_environment(part_dependencies=["rust-deps"])
 
-    @pytest.mark.parametrize(
-        "satisfied_dependency,error_dependency",
-        [
-            ("cargo", "rustc"),
-            ("rustc", "cargo"),
-        ],
+
+def test_get_pull_commands_compat(part_info):
+    properties = RustPlugin.properties_class.unmarshal(
+        {"source": ".", "after": ["rust-deps"]}
     )
-    def test_validate_environment_missing_part_dependencies(
-        self,
-        satisfied_dependency,
-        error_dependency,
-        dependency_fixture,
-        new_dir,
-        part_info,
-    ):
-        """Validate that missing part dependencies raise an exception.
+    plugin = RustPlugin(properties=properties, part_info=part_info)
+    plugin._check_rustup = lambda: False
 
-        :param dependencies: tuple consisting of 1 missing part dependency
-        and a list of valid part dependencies
-        """
-        dependency = dependency_fixture(name=satisfied_dependency)
+    commands = plugin.get_build_commands()
+    assert plugin.get_pull_commands() == []
+    assert 'cargo install -f --locked --path "."' in commands[0]
 
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
 
-        validator = plugin.validator_class(
-            part_name="my-part",
-            properties=properties,
-            env=f"PATH={str(dependency.parent)}",
-        )
-        with pytest.raises(errors.PluginEnvironmentValidationError) as raised:
-            validator.validate_environment(part_dependencies=[])
+def test_get_out_of_source_build(part_info):
+    properties = RustPlugin.properties_class.unmarshal({"source": "."})
+    plugin = RustPlugin(properties=properties, part_info=part_info)
 
-        assert raised.value.reason == (
-            f"{error_dependency!r} not found and part 'my-part' "
-            "does not depend on a part named 'rust-deps' that "
-            "would satisfy the dependency"
-        )
-
-    def test_get_build_snaps(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-        assert plugin.get_build_snaps() == set()
-
-    def test_get_build_packages(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-        assert plugin.get_build_packages() == {"gcc", "git"}
-
-    def test_get_build_environment(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-
-        assert plugin.get_build_environment() == {
-            "PATH": "${HOME}/.cargo/bin:${PATH}",
-        }
-
-    def test_get_build_commands(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-
-        assert plugin.get_build_commands() == [
-            'cargo install --locked --path . --root "${CRAFT_PART_INSTALL}" --force',
-        ]
-
-    def test_get_build_commands_rust_features(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal(
-            {"source": ".", "rust-features": ["test-feature-1", "test-feature-2"]}
-        )
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-
-        assert plugin.get_build_commands() == [
-            'cargo install --locked --path . --root "${CRAFT_PART_INSTALL}" --force '
-            "--features 'test-feature-1 test-feature-2'",
-        ]
-
-    @pytest.mark.parametrize(
-        "rust_features",
-        [None, "i am a string", {"i am": "a dictionary"}, ["duplicate", "duplicate"]],
-    )
-    def test_get_build_commands_rust_features_invalid(self, rust_features, part_info):
-        with pytest.raises(ValidationError):
-            RustPlugin.properties_class.unmarshal(
-                {"source": ".", "rust-features": rust_features}
-            )
-
-    def test_get_build_commands_rust_path(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal(
-            {"source": ".", "rust-path": ["test-path-1", "test-path-2"]}
-        )
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-
-        assert plugin.get_build_commands() == [
-            "cargo install --locked --path test-path-1"
-            ' --root "${CRAFT_PART_INSTALL}" --force',
-        ]
-
-    @pytest.mark.parametrize(
-        "rust_path",
-        [None, "i am a string", {"i am": "a dictionary"}, ["duplicate", "duplicate"]],
-    )
-    def test_get_build_commands_rust_path_invalid(self, rust_path, part_info):
-        with pytest.raises(ValidationError):
-            RustPlugin.properties_class.unmarshal(
-                {"source": ".", "rust-path": rust_path}
-            )
-
-    def test_get_out_of_source_build(self, part_info):
-        properties = RustPlugin.properties_class.unmarshal({"source": "."})
-        plugin = RustPlugin(properties=properties, part_info=part_info)
-
-        assert plugin.get_out_of_source_build() is False
+    assert plugin.get_out_of_source_build() is False
