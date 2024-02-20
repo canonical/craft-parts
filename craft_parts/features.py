6e076810a6189c9ae54427e3f3c5893f9f2104e5
Callahan Kovacs <callahan.kovacs@canonical.com>
2024-02-20 09:23:37 -0600

    fix: make overlay and partition features mutually exclusive
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- craft_parts/features.py
+++ craft_parts/features.py
@@ -1,6 +1,6 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright 2023 Canonical Ltd.
+# Copyright 2023-2024 Canonical Ltd.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -20,6 +20,7 @@ import contextlib
 import dataclasses
 import logging
 
+from craft_parts.errors import FeatureError
 from craft_parts.utils import Singleton
 
 logger = logging.getLogger()
@@ -35,6 +36,24 @@ class Features(metaclass=Singleton):
 
     enable_overlay: bool = False
     enable_partitions: bool = False
+
+    def __init__(
+        self, *, enable_overlay: bool = False, enable_partitions: bool = False
+    ) -> None:
+        """Initialize craft-parts features.
+
+        :param enable_overlay: Enables the overlay step.
+        :param enable_partitions: Enables the usage of partitions.
+
+        :raises FeatureError: If mutually exclusive features are enabled.
+        """
+        if enable_overlay and enable_partitions:
+            raise FeatureError(
+                message="Cannot enable overlay and partition features.",
+                details=("Overlay and partition features are mutually exclusive."),
+            )
+        object.__setattr__(self, "enable_overlay", enable_overlay)
+        object.__setattr__(self, "enable_partitions", enable_partitions)
 
     @classmethod
     def reset(cls) -> None:
