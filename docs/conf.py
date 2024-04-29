278feb8db0b778499291028354824f225314d6cb
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-04-29 10:41:47 -0300

    docs: use canonical-sphinx (#720)

--- docs/conf.py
+++ docs/conf.py
@@ -1,79 +1,56 @@
-# Copyright 2021 Canonical Ltd.
+# Copyright 2023-2024 Canonical Ltd.
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
-# published by the Free Software Foundation.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+import datetime
 import logging
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(".."))
-
-
-# -- Project information -----------------------------------------------------
-
 project = "Craft Parts"
-copyright = "2023, Canonical Ltd."
-author = "Canonical Ltd."
+author = "Canonical Group Ltd"
 
-# The full version, including alpha/beta/rc tags
-release = "1.29.0"
+copyright = "2023-%s, %s" % (datetime.date.today().year, author)
 
-# Open Graph configuration - defines what is displayed in the website preview
-ogp_site_url = "https://canonical-craft-parts.readthedocs-hosted.com"
+# region Configuration for canonical-sphinx
+ogp_site_url = "https://canonical-craft-parts.readthedocs-hosted.com/"
 ogp_site_name = project
-ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
-
-# Update with the favicon for your product
-html_favicon = "sphinx-resources/.sphinx/_static/favicon.png"
 
 html_context = {
     "product_page": "github.com/canonical/craft-parts",
-    "product_tag": "sphinx-resources/.sphinx/_static/tag.png",
     "github_url": "https://github.com/canonical/craft-parts",
-    "github_version": "main",
-    "github_folder": "/docs/",
-    "github_issues": "enabled",
 }
 
-# -- General configuration ---------------------------------------------------
-
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-    "sphinx_autodoc_typehints",  # must be loaded after napoleon
-    "sphinx-pydantic",
-    "sphinx_design",
-    "sphinxcontrib.details.directive",
+    "canonical_sphinx",
 ]
+# endregion
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+extensions.extend(
+    [
+        "sphinx.ext.autodoc",
+        "sphinx.ext.autosummary",
+        "sphinx.ext.ifconfig",
+        "sphinx.ext.napoleon",
+        "sphinx.ext.viewcode",
+        "sphinx_autodoc_typehints",  # must be loaded after napoleon
+        "sphinx-pydantic",
+        "sphinxcontrib.details.directive",
+    ]
+)
+
+# region Options for extensions
+
 exclude_patterns = [
     "_build",
     "Thumbs.db",
@@ -81,6 +58,7 @@ exclude_patterns = [
     "**venv",
     "base",
     "sphinx-resources",
+    "common/README.md",
     # These RST files are explicitly excluded here because they are included by
     # other files - without this exclusion, Sphinx will complain about duplicate
     # labels.
@@ -90,116 +68,18 @@ exclude_patterns = [
     "common/craft-parts/reference/step_output_directories.rst",
 ]
 
+# Links to ignore when checking links
+linkcheck_ignore = [
+    "https://foo.org/",
+]
+
 rst_epilog = """
 .. include:: /common/craft-parts/reuse/links.txt
 """
 
 autodoc_mock_imports = ["apt"]
 
-# Links to ignore when checking links
-linkcheck_ignore = [
-    "https://foo.org/",
-]
-
-rst_prolog = """
-.. |br| raw:: html
-
-   <br />
-"""
-
-disable_feedback_button = False
-
-# -- Options for HTML output -------------------------------------------------
-
-# Find the current builder
-builder = "dirhtml"
-if "-b" in sys.argv:
-    builder = sys.argv[sys.argv.index("-b") + 1]
-
-# Setting templates_path for epub makes the build fail
-if builder == "dirhtml" or builder == "html":
-    templates_path = ["sphinx-resources/.sphinx/_templates"]
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "furo"
-html_last_updated_fmt = ""
-html_permalinks_icon = "¶"
-html_theme_options = {
-    "light_css_variables": {
-        "color-sidebar-background-border": "none",
-        "font-stack": "Ubuntu, -apple-system, Segoe UI, Roboto, Oxygen, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
-        "font-stack--monospace": "Ubuntu Mono, Consolas, Monaco, Courier, monospace",
-        "color-foreground-primary": "#111",
-        "color-foreground-secondary": "var(--color-foreground-primary)",
-        "color-foreground-muted": "#333",
-        "color-background-secondary": "#FFF",
-        "color-background-hover": "#f2f2f2",
-        "color-brand-primary": "#111",
-        "color-brand-content": "#06C",
-        "color-api-background": "#cdcdcd",
-        "color-inline-code-background": "rgba(0,0,0,.03)",
-        "color-sidebar-link-text": "#111",
-        "color-sidebar-item-background--current": "#ebebeb",
-        "color-sidebar-item-background--hover": "#f2f2f2",
-        "toc-font-size": "var(--font-size--small)",
-        "color-admonition-title-background--note": "var(--color-background-primary)",
-        "color-admonition-title-background--tip": "var(--color-background-primary)",
-        "color-admonition-title-background--important": "var(--color-background-primary)",
-        "color-admonition-title-background--caution": "var(--color-background-primary)",
-        "color-admonition-title--note": "#24598F",
-        "color-admonition-title--tip": "#24598F",
-        "color-admonition-title--important": "#C7162B",
-        "color-admonition-title--caution": "#F99B11",
-        "color-highlighted-background": "#EbEbEb",
-        "color-link-underline": "var(--color-background-primary)",
-        "color-link-underline--hover": "var(--color-background-primary)",
-        "color-version-popup": "#772953",
-    },
-    "dark_css_variables": {
-        "color-foreground-secondary": "var(--color-foreground-primary)",
-        "color-foreground-muted": "#CDCDCD",
-        "color-background-secondary": "var(--color-background-primary)",
-        "color-background-hover": "#666",
-        "color-brand-primary": "#fff",
-        "color-brand-content": "#06C",
-        "color-sidebar-link-text": "#f7f7f7",
-        "color-sidebar-item-background--current": "#666",
-        "color-sidebar-item-background--hover": "#333",
-        "color-admonition-background": "transparent",
-        "color-admonition-title-background--note": "var(--color-background-primary)",
-        "color-admonition-title-background--tip": "var(--color-background-primary)",
-        "color-admonition-title-background--important": "var(--color-background-primary)",
-        "color-admonition-title-background--caution": "var(--color-background-primary)",
-        "color-admonition-title--note": "#24598F",
-        "color-admonition-title--tip": "#24598F",
-        "color-admonition-title--important": "#C7162B",
-        "color-admonition-title--caution": "#F99B11",
-        "color-highlighted-background": "#666",
-        "color-link-underline": "var(--color-background-primary)",
-        "color-link-underline--hover": "var(--color-background-primary)",
-        "color-version-popup": "#F29879",
-    },
-}
-
-html_static_path = ["sphinx-resources/.sphinx/_static"]
-html_css_files = [
-    "custom.css",
-    "header.css",
-    "github_issue_links.css",
-]
-
-html_js_files = ["header-nav.js"]
-if (
-    "github_issues" in html_context
-    and html_context["github_issues"]
-    and not disable_feedback_button
-):
-    html_js_files.append("github_issue_links.js")
-
-
-# Do (not) include module names.
+# Type annotations config
 add_module_names = True
 
 # sphinx_autodoc_typehints
@@ -210,6 +90,12 @@ typehints_document_rtype = True
 
 # Enable support for google-style instance attributes.
 napoleon_use_ivar = True
+
+# Github config
+github_username = "canonical"
+github_repository = "craft-parts"
+
+# endregion
 
 
 def run_apidoc(_):
