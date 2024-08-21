6eeb6dcfd56bb12d0f8a92f2a050f8aca7167c7e
Matt Culler <matt.culler@canonical.com>
2024-08-21 16:47:55 -0400

    chore: deduplicate checks and make type checker happy

--- craft_parts/dirs.py
+++ craft_parts/dirs.py
@@ -20,7 +20,7 @@ from collections.abc import Sequence
 from pathlib import Path
 from types import MappingProxyType
 
-from craft_parts.errors import PartitionNotFound
+from craft_parts.errors import PartitionNotFound, PartitionUsageError
 from craft_parts.utils import partition_utils
 
 
@@ -75,14 +75,24 @@ class ProjectDirs:
             )
         )
 
+    def _validate_requested_partition(
+        self, dir_name: str, partition: str | None = None
+    ) -> None:
+        """Ensure the requested partition is valid."""
+        if self._partitions:
+            if not partition:
+                raise PartitionUsageError(
+                    f"Partitions are enabled, you must specify which partition's {dir_name} you want."
+                )
+            if partition not in self._partitions:
+                raise PartitionNotFound(partition, self._partitions)
+
     def get_stage_dir(self, partition: str | None = None) -> Path:
         """Get the stage directory for the given partition."""
-        if self._partitions and partition not in self._partitions:
-            raise PartitionNotFound(partition, self._partitions)
+        self._validate_requested_partition("stage_dir", partition)
         return self.stage_dirs[partition]
 
     def get_prime_dir(self, partition: str | None = None) -> Path:
-        """Get the stage directory for the given partition."""
-        if self._partitions and partition not in self._partitions:
-            raise PartitionNotFound(partition, self._partitions)
+        """Get the prime directory for the given partition."""
+        self._validate_requested_partition("prime_dir", partition)
         return self.prime_dirs[partition]
