f36794342c63faeaeaf44e8c54327f6e72dee49e
Alex Lowe <alex.lowe@canonical.com>
2024-08-22 14:24:07 -0400

    feat(plugins): create base Python plugin (#819)

--- craft_parts/plugins/base.py
+++ craft_parts/plugins/base.py
@@ -19,8 +19,11 @@
 from __future__ import annotations
 
 import abc
+import textwrap
 from copy import deepcopy
 from typing import TYPE_CHECKING
+
+from overrides import override
 
 from craft_parts.actions import ActionProperties
 
@@ -122,3 +125,170 @@ class JavaPlugin(Plugin):
         # pylint: enable=line-too-long
 
         return link_java + link_jars
+
+
+class BasePythonPlugin(Plugin):
+    """A base class for Python plugins.
+
+    Provides common methods for dealing with Python items.
+    """
+
+    @override
+    def get_build_snaps(self) -> set[str]:
+        """Return a set of required snaps to install in the build environment."""
+        return set()
+
+    @override
+    def get_build_packages(self) -> set[str]:
+        """Return a set of required packages to install in the build environment.
+
+        Child classes that need to override this should extend the returned set.
+        """
+        return {"findutils", "python3-dev", "python3-venv"}
+
+    @override
+    def get_build_environment(self) -> dict[str, str]:
+        """Return a dictionary with the environment to use in the build step.
+
+        Child classes that need to override this should extend the dictionary returned
+        by this class.
+        """
+        return {
+            # Add PATH to the python interpreter we always intend to use with
+            # this plugin. It can be user overridden, but that is an explicit
+            # choice made by a user.
+            "PATH": f"{self._part_info.part_install_dir}/bin:${{PATH}}",
+            "PARTS_PYTHON_INTERPRETER": "python3",
+            "PARTS_PYTHON_VENV_ARGS": "",
+        }
+
+    def _get_create_venv_commands(self) -> list[str]:
+        """Get the commands for setting up the virtual environment."""
+        return [
+            f'"${{PARTS_PYTHON_INTERPRETER}}" -m venv ${{PARTS_PYTHON_VENV_ARGS}} "{self._part_info.part_install_dir}"',
+            f'PARTS_PYTHON_VENV_INTERP_PATH="{self._part_info.part_install_dir}/bin/${{PARTS_PYTHON_INTERPRETER}}"',
+        ]
+
+    def _get_find_python_interpreter_commands(self) -> list[str]:
+        """Get the commands that find a staged Python interpreter.
+
+        These commands should, in bash, have a side-effect of creating a variable
+        called ``symlink_target`` containing the path to the relevant Python payload.
+        """
+        python_interpreter = self._get_system_python_interpreter() or ""
+        return [
+            textwrap.dedent(
+                f"""\
+            # look for a provisioned python interpreter
+            opts_state="$(set +o|grep errexit)"
+            set +e
+            install_dir="{self._part_info.part_install_dir}/usr/bin"
+            stage_dir="{self._part_info.stage_dir}/usr/bin"
+
+            # look for the right Python version - if the venv was created with python3.10,
+            # look for python3.10
+            basename=$(basename $(readlink -f ${{PARTS_PYTHON_VENV_INTERP_PATH}}))
+            echo Looking for a Python interpreter called \\"${{basename}}\\" in the payload...
+            payload_python=$(find "$install_dir" "$stage_dir" -type f -executable -name "${{basename}}" -print -quit 2>/dev/null)
+
+            if [ -n "$payload_python" ]; then
+                # We found a provisioned interpreter, use it.
+                echo Found interpreter in payload: \\"${{payload_python}}\\"
+                installed_python="${{payload_python##{self._part_info.part_install_dir}}}"
+                if [ "$installed_python" = "$payload_python" ]; then
+                    # Found a staged interpreter.
+                    symlink_target="..${{payload_python##{self._part_info.stage_dir}}}"
+                else
+                    # The interpreter was installed but not staged yet.
+                    symlink_target="..$installed_python"
+                fi
+            else
+                # Otherwise use what _get_system_python_interpreter() told us.
+                echo "Python interpreter not found in payload."
+                symlink_target="{python_interpreter}"
+            fi
+
+            if [ -z "$symlink_target" ]; then
+                echo "No suitable Python interpreter found, giving up."
+                exit 1
+            fi
+
+            eval "${{opts_state}}"
+            """
+            )
+        ]
+
+    def _get_rewrite_shebangs_commands(self) -> list[str]:
+        """Get the commands used to rewrite shebangs in the install dir.
+
+        This can be overridden by application-specific subclasses to control how Python
+        shebangs in the final environment should be handled.
+        """
+        script_interpreter = self._get_script_interpreter()
+        return [
+            textwrap.dedent(
+                f"""\
+                find "{self._part_info.part_install_dir}" -type f -executable -print0 | xargs -0 \\
+                    sed -i "1 s|^#\\!${{PARTS_PYTHON_VENV_INTERP_PATH}}.*$|{script_interpreter}|"
+                """
+            )
+        ]
+
+    def _get_handle_symlinks_commands(self) -> list[str]:
+        """Get commands for handling Python symlinks."""
+        if self._should_remove_symlinks():
+            return [
+                f"echo Removing python symlinks in {self._part_info.part_install_dir}/bin",
+                f'rm "{self._part_info.part_install_dir}"/bin/python*',
+            ]
+        return ['ln -sf "${symlink_target}" "${PARTS_PYTHON_VENV_INTERP_PATH}"']
+
+    def _should_remove_symlinks(self) -> bool:
+        """Configure executables symlink removal.
+
+        This method can be overridden by application-specific subclasses to control
+        whether symlinks in the virtual environment should be removed. Default is
+        False.  If True, the venv-created symlinks to python* in bin/ will be
+        removed and will not be recreated.
+        """
+        return False
+
+    def _get_system_python_interpreter(self) -> str | None:
+        """Obtain the path to the system-provided python interpreter.
+
+        This method can be overridden by application-specific subclasses. It
+        returns the path to the Python that bin/python3 should be symlinked to
+        if Python is not part of the payload.
+        """
+        return '$(readlink -f "$(which "${PARTS_PYTHON_INTERPRETER}")")'
+
+    def _get_script_interpreter(self) -> str:
+        """Obtain the shebang line to use in Python scripts.
+
+        This method can be overridden by application-specific subclasses. It
+        returns the script interpreter to use in existing Python scripts.
+        """
+        return "#!/usr/bin/env ${PARTS_PYTHON_INTERPRETER}"
+
+    def _get_pip(self) -> str:
+        """Get the pip command to use."""
+        return f"{self._part_info.part_install_dir}/bin/pip"
+
+    @abc.abstractmethod
+    def _get_package_install_commands(self) -> list[str]:
+        """Get the commands for installing the given package in the Python virtualenv.
+
+        A specific Python build system plugin should override this method to provide
+        the necessary commands.
+        """
+
+    @override
+    def get_build_commands(self) -> list[str]:
+        """Return a list of commands to run during the build step."""
+        return [
+            *self._get_create_venv_commands(),
+            *self._get_package_install_commands(),
+            *self._get_rewrite_shebangs_commands(),
+            *self._get_find_python_interpreter_commands(),
+            *self._get_handle_symlinks_commands(),
+        ]
