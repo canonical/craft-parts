f697bc081424debede261734633f7d330bfdc100
Alex Lowe <alex.lowe@canonical.com>
2024-08-23 17:51:10 -0400

    lint: typing in poetry integration test

--- tests/integration/plugins/test_poetry/test_poetry/__init__.py
+++ tests/integration/plugins/test_poetry/test_poetry/__init__.py
@@ -1,8 +1,8 @@
-import distro
+import distro  # type: ignore[import-not-found]
 
 
 def main() -> int:
     # Check that we installed the correct version of `distro`.
-    assert distro.__version__ == "1.8.0"
+    assert distro.__version__ == "1.8.0"  # pyright: ignore[reportPrivateImportUsage]
     print("Test succeeded!")
     return 0
