42eecd43c1395bf7a6ad614d3f958e21aa46161e
Alex Lowe <alex.lowe@canonical.com>
2024-09-30 14:51:14 -0400

    style: adjust pylint configuration to make lint pass

--- craft_parts/lifecycle_manager.py
+++ craft_parts/lifecycle_manager.py
@@ -83,7 +83,7 @@ class LifecycleManager:
         to :ref:`callbacks<callbacks>`.
     """
 
-    def __init__(
+    def __init__(  # pylint: disable=too-many-arguments
         self,
         all_parts: Dict[str, Any],
         *,
