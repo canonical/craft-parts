704533062f9c1a4d71b8df74b7d152cef551a7ac
Matt Culler <matt.culler@canonical.com>
2024-12-12 20:38:46 -0500

    feat: start of integration test

--- tests/integration/plugins/test_npm.py
+++ tests/integration/plugins/test_npm.py
@@ -18,21 +18,12 @@ import subprocess
 import textwrap
 from pathlib import Path
 
+import pytest
 import yaml
 from craft_parts import LifecycleManager, Step
 
 
-def test_npm_plugin(new_dir, partitions):
-    parts_yaml = textwrap.dedent(
-        """\
-        parts:
-          foo:
-            plugin: npm
-            source: .
-        """
-    )
-    parts = yaml.safe_load(parts_yaml)
-
+def _mk_hello_js():
     Path("hello.js").write_text(
         textwrap.dedent(
             """\
@@ -43,38 +34,61 @@ def test_npm_plugin(new_dir, partitions):
         )
     )
 
-    Path("package.json").write_text(
-        textwrap.dedent(
+
+@pytest.fixture
+def create_fake_package():
+    def _create_fake_package():
+        parts_yaml = textwrap.dedent(
             """\
-            {
-              "name": "npm-hello",
-              "version": "1.0.0",
-              "description": "Testing grounds for snapcraft integration tests",
-              "bin": {
-                "npm-hello": "hello.js"
-              },
-              "scripts": {
-                "npm-hello": "echo 'Error: no test specified' && exit 1"
-              },
-              "author": "",
-              "license": "GPL-3.0"
-            }
+            parts:
+              foo:
+                plugin: npm
+                source: .
+                npm-include-node: True
+                npm-node-version: "16.14.2"
             """
         )
-    )
+        parts = yaml.safe_load(parts_yaml)
 
-    Path("package-lock.json").write_text(
-        textwrap.dedent(
-            """\
-            {
-              "name": "npm-hello",
-              "version": "1.0.0",
-              "lockfileVersion": 1
-            }
-            """
+        _mk_hello_js()
+
+        Path("package.json").write_text(
+            textwrap.dedent(
+                """\
+                {
+                  "name": "npm-hello",
+                  "version": "1.0.0",
+                  "description": "Testing grounds for snapcraft integration tests",
+                  "bin": {
+                    "npm-hello": "hello.js"
+                  },
+                  "scripts": {
+                    "npm-hello": "echo 'Error: no test specified' && exit 1"
+                  },
+                  "author": "",
+                  "license": "GPL-3.0"
+                }
+                """
+            )
         )
-    )
 
+        Path("package-lock.json").write_text(
+            textwrap.dedent(
+                """\
+                {
+                  "name": "npm-hello",
+                  "version": "1.0.0",
+                  "lockfileVersion": 1
+                }
+                """
+            )
+        )
+        return parts
+    return _create_fake_package
+
+
+def test_npm_plugin(create_fake_package, new_dir, partitions):
+    parts = create_fake_package()
     lifecycle = LifecycleManager(
         parts,
         application_name="test_npm_plugin",
@@ -94,61 +108,8 @@ def test_npm_plugin(new_dir, partitions):
     assert Path(lifecycle.project_info.prime_dir, "bin", "node").exists() is False
 
 
-def test_npm_plugin_include_node(new_dir, partitions):
-    parts_yaml = textwrap.dedent(
-        """\
-        parts:
-          foo:
-            plugin: npm
-            source: .
-            npm-include-node: True
-            npm-node-version: "16.14.2"
-        """
-    )
-    parts = yaml.safe_load(parts_yaml)
-
-    Path("hello.js").write_text(
-        textwrap.dedent(
-            """\
-            #!/usr/bin/env node
-
-            console.log('hello world');
-            """
-        )
-    )
-
-    Path("package.json").write_text(
-        textwrap.dedent(
-            """\
-            {
-              "name": "npm-hello",
-              "version": "1.0.0",
-              "description": "Testing grounds for snapcraft integration tests",
-              "bin": {
-                "npm-hello": "hello.js"
-              },
-              "scripts": {
-                "npm-hello": "echo 'Error: no test specified' && exit 1"
-              },
-              "author": "",
-              "license": "GPL-3.0"
-            }
-            """
-        )
-    )
-
-    Path("package-lock.json").write_text(
-        textwrap.dedent(
-            """\
-            {
-              "name": "npm-hello",
-              "version": "1.0.0",
-              "lockfileVersion": 1
-            }
-            """
-        )
-    )
-
+def test_npm_plugin_include_node(create_fake_package, new_dir, partitions):
+    parts = create_fake_package()
     lifecycle = LifecycleManager(
         parts,
         application_name="test_npm_plugin",
@@ -166,3 +127,44 @@ def test_npm_plugin_include_node(new_dir, partitions):
     # try to use bundled Node.js to execute the script
     output = subprocess.check_output([str(node_path), str(binary)], text=True)
     assert output == "hello world\n"
+
+
+def test_npm_plugin_get_file_list_simple(create_fake_package, new_dir, partitions):
+    parts = create_fake_package()
+    lifecycle = LifecycleManager(
+        parts,
+        application_name="test_npm_plugin",
+        cache_dir=new_dir,
+        partitions=partitions,
+    )
+    actions = lifecycle.plan(Step.BUILD)
+
+    with lifecycle.action_executor() as ctx:
+        ctx.execute(actions)
+
+    part_name = list(parts["parts"].keys())[0]
+    actual_file_list = lifecycle._executor._handler[part_name]._plugin.get_file_list()
+
+    
+
+    breakpoint()
+
+    binary = Path(lifecycle.project_info.prime_dir, "bin", "npm-hello")
+
+    output = subprocess.check_output([str(binary)], text=True)
+    assert output == "hello world\n"
+
+    assert Path(lifecycle.project_info.prime_dir, "bin", "node").exists() is False
+
+
+#def test_npm_plugin_get_file_list_complex(
+#
+#    parts_yaml = textwrap.dedent(
+#        """\
+#        parts:
+#          foob:
+#            plugin: npm
+#            source: https://github.com/animate-object/is-ten
+#        """
+#    )
+#    parts = yaml.safe_load(parts_yaml)
