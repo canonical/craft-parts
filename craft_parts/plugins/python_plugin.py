18deeb08b3bc93d623b6113b09d870ca4d398105
Matt Culler <matt.culler@canonical.com>
2024-12-16 14:43:30 -0500

    feat: add plugin code from prototype with signature from npm PR

--- craft_parts/plugins/python_plugin.py
+++ craft_parts/plugins/python_plugin.py
@@ -19,7 +19,7 @@
 import shlex
 from typing import Literal
 
-from .base import BasePythonPlugin
+from .base import BasePythonPlugin, Package, PackageFiles
 from .properties import PluginProperties
 
 
@@ -73,3 +73,43 @@ class PythonPlugin(BasePythonPlugin):
         )
 
         return commands
+
+    
+    @override
+    def get_files(self) -> PackageFiles:
+        # https://packaging.python.org/en/latest/specifications/binary-distribution-format/
+        # Read the RECORD files
+
+        venvdir = self._get_venv_directory()
+        python_path = venvdir / "bin/python"
+        python_version = python_path.resolve().name
+
+        site_pkgs_dir = venvdir / "lib" / python_version / "site-packages"
+
+        # Could also add the pkginfo library and skip a lot of the below
+
+        ret = {}
+        for pkg_dir in site_pkgs_dir.iterdir():
+            # We only care about the metadata dirs
+            if not pkg_dir.name.endswith(".dist-info"):
+                continue
+
+            # Get package name and version from filename - could also parse it from METADATA
+            # I assume there must be at least one character for version, not sure what else it could look like though.  I've seen:
+            # - 0.0.0
+            # - 0.0
+            # TODO: look this up
+            pkg_name_match = re.match(r"^(.*)-[0-9.]+\.dist-info$", pkg_dir.name)
+            if not pkg_name_match:
+                raise Exception(f"Unexpectedly formatted dist-info dir: {pkg_dir.name!r}")
+            pkg_name = pkg_name_match[1]
+
+            record_file = pkg_dir / "RECORD"
+            with open(record_file, "r") as f:
+                csvreader = csv.reader(f)
+
+                # First row is files
+                # TODO: Remove all files listed under the dist-info dir?
+                pkg_files = [Path(f[0]) for f in csvreader]
+                ret[pkg_name] = pkg_files
+        return ret
