d498a6d8c8eb230681b1a193bec86729924081c3
Alex Lowe <alex.lowe@canonical.com>
2025-01-22 16:45:54 -0500

    tests(cargo-package): integration tests for cargo-package
    These tests show that we're putting stuff on the backstage

--- tests/integration/plugins/test_cargo_package.py
+++ tests/integration/plugins/test_cargo_package.py
@@ -17,12 +17,14 @@
 import pathlib
 import subprocess
 import textwrap
+from typing import cast
 
 import pytest
 import yaml
 from craft_parts import LifecycleManager, Step
+from craft_parts.errors import PluginBuildError
 
-CARGO_PARTS_YAML = """\
+CARGO_PARTS_YAML = f"""\
 parts:
   rust-ascii:
     plugin: cargo-package
@@ -33,7 +35,31 @@ parts:
   ascii-consumer:  # Test that we can correctly consume the package from another part.
     after: [rust-ascii]
     plugin: rust
-    source: .
+    source: {pathlib.Path(__file__).parent / "test_cargo_package"}
+    rust-channel: none
+    build-environment:
+      # Should not be necessary as the other part sets offline, but do so anyway
+      # so we don't use an online version if we change this.
+      - CARGO_NET_OFFLINE: "true"
+      # Not necessary as the other part overrides crates.io, but override it anyway
+      # to ensure forward compatibility.
+      - CARGO_REGISTRY_DEFAULT: "craft-parts"
+      # Don't use snaps first.
+      - PATH: /usr/bin:$PATH
+"""
+
+BROKEN_CARGO_PARTS_YAML = f"""\
+parts:
+  rust-ascii:
+    plugin: cargo-package
+    source: https://github.com/tomprogrammer/rust-ascii.git
+    source-tag: v1.1.0  # This will work, but the consumer will break because it wants an old version.
+    # Use the system's cargo, not cargo from the snap.
+    cargo-package-cargo-command: /usr/bin/cargo
+  ascii-consumer:  # Test that we can correctly consume the package from another part.
+    after: [rust-ascii]
+    plugin: rust
+    source: {pathlib.Path(__file__).parent / "test_cargo_package"}
     rust-channel: none
     build-environment:
       # Should not be necessary as the other part sets offline, but do so anyway
@@ -56,39 +82,37 @@ def fake_homedir(
     return tmp_path
 
 
+def test_cargo_package_plugin_goes_to_backstage(new_path, partitions):
+    parts = yaml.safe_load((pathlib.Path(__file__).parent / "test_cargo_package/parts.yaml").read_text())
+
+    lifecycle = LifecycleManager(
+        parts,
+        application_name="test_cargo_package_plugin",
+        cache_dir=new_path.as_posix(),
+        partitions=partitions,
+    )
+    actions = lifecycle.plan(Step.BUILD, part_names=["root"])
+
+    with lifecycle.action_executor() as ctx:
+        ctx.execute(actions)
+
+    # Nothing in the backstage yet
+    assert list(lifecycle.project_info.backstage_dir.rglob("*")) == []
+    # But we have stuff to export.
+    assert (new_path / "parts/root/export/cargo_registry/ascii-1.1.0/.cargo-checksum.json").exists()
+
+    actions = lifecycle.plan(Step.STAGE)
+    with lifecycle.action_executor() as ctx:
+        ctx.execute(actions)
+
+    # Nothing staged
+    assert list(lifecycle.project_info.stage_dir.rglob("*")) == []
+    # But we do have stuff on the backstage
+    assert (new_path / "backstage/cargo_registry/ascii-1.1.0/Cargo.toml").exists()
+
+
 def test_cargo_package_plugin_with_rust_consumer(new_dir, partitions):
     parts = yaml.safe_load(CARGO_PARTS_YAML)
-
-    (new_dir / "Cargo.toml").write_text(
-        textwrap.dedent(
-            """\
-                [package]
-                name = "package-test"
-                version = "0.1.0"
-                edition = "2021"
-
-                [dependencies]
-                ascii = "0.8.7"
-            """
-        ),
-        encoding="utf-8",
-    )
-    (new_dir / "src").mkdir()
-    (new_dir / "src" / "main.rs").write_text(
-        textwrap.dedent(
-            """\
-                use ascii::{AsciiString};
-                fn main() {
-                    let my_str = match AsciiString::from_ascii("Hello, world!") {
-                        Ok(my_str) => my_str,
-                        Err(_) => todo!(),
-                    };
-                    println!("{}", my_str);
-                }
-            """
-        ),
-        encoding="utf-8",
-    )
 
     lifecycle = LifecycleManager(
         parts,
@@ -108,3 +132,30 @@ def test_cargo_package_plugin_with_rust_consumer(new_dir, partitions):
 
     binary_contents = binary.read_bytes()
     assert b"AsciiString" in binary_contents, "binary does not reference AsciiString"
+
+
+def test_cargo_package_plugin_with_wrong_version(new_dir, partitions):
+    parts = yaml.safe_load(BROKEN_CARGO_PARTS_YAML)
+
+    lifecycle = LifecycleManager(
+        parts,
+        application_name="test_cargo_package_plugin",
+        cache_dir=new_dir,
+        partitions=partitions,
+    )
+    actions = lifecycle.plan(Step.STAGE, part_names=["rust-ascii"])
+
+    with lifecycle.action_executor() as ctx:
+        ctx.execute(actions)
+
+    actions = lifecycle.plan(Step.STAGE, part_names=["ascii-consumer"])
+
+    with lifecycle.action_executor() as ctx:
+        with pytest.raises(PluginBuildError) as exc_info:
+            ctx.execute(actions)
+
+    err = cast(bytes, exc_info.value.stderr)
+
+    assert b'failed to select a version for the requirement `ascii = "^0.8.7"`' in err
+    assert b"candidate versions found which didn't match: 1.1.0" in err
+
