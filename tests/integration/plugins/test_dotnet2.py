afa55015d45c5e9a1a64018598066cffe592d1e2
Mateus Rodrigues de Morais <mateus.morais@canonical.com>
2025-02-28 17:15:16 -0300

    test: add integration tests to dotnet2 plugin

---
+++ tests/integration/plugins/test_dotnet2.py
@@ -0,0 +1,134 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2021 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import subprocess
+import textwrap
+from pathlib import Path
+
+import yaml
+from craft_parts import LifecycleManager, Step
+from craft_parts.plugins import dotnet2_plugin
+from overrides import override
+
+
+def test_dotnet2_plugin(new_dir, partitions):
+    # pylint: disable=line-too-long
+    parts_yaml = textwrap.dedent(
+        """
+        parts:
+          foo:
+            source: .
+            plugin: dotnet2
+            dotnet2-self-contained: true
+            build-environment:
+              - PATH: $CRAFT_STAGE/sdk:$PATH
+            after: [dotnet2-deps]
+          dotnet2-deps:
+            plugin: dump
+            source: https://download.visualstudio.microsoft.com/download/pr/d2abdb4c-a96e-4123-9351-e4dd2ea20905/e8010ae2688786ffc1ebca4ebb52f41b/dotnet-sdk-8.0.406-linux-x64.tar.gz
+            source-checksum: sha512/d6fdcfebd0df46959f7857cfb3beac7de6c8843515ece28b24802765fd9cfb6c7e9701b320134cb4907322937ab89cae914ddc21bf48b9b6313e9a9af5c1f24a
+            override-build: |
+              # TODO: find out why this is a problem with "organize".XS
+              cp --archive --link --no-dereference . $CRAFT_PART_INSTALL/sdk
+            prime:
+              - -sdk
+        """
+    )
+    # pylint: enable=line-too-long
+    parts = yaml.safe_load(parts_yaml)
+
+    Path("dotnet.csproj").write_text(
+        textwrap.dedent(
+            """
+            <Project Sdk="Microsoft.NET.Sdk">
+
+              <PropertyGroup>
+                <OutputType>Exe</OutputType>
+                <TargetFramework>net8.0</TargetFramework>
+                <ImplicitUsings>enable</ImplicitUsings>
+                <Nullable>enable</Nullable>
+                <RuntimeIdentifier>linux-x64</RuntimeIdentifier>
+              </PropertyGroup>
+
+            </Project>
+            """
+        )
+    )
+
+    Path("hello.cs").write_text('Console.WriteLine("Hello, World!");')
+
+    lf = LifecycleManager(
+        parts, application_name="test_dotnet", cache_dir=new_dir, partitions=partitions
+    )
+    actions = lf.plan(Step.PRIME)
+
+    with lf.action_executor() as ctx:
+        ctx.execute(actions)
+
+    binary = Path(lf.project_info.prime_dir, "dotnet")
+
+    output = subprocess.check_output([str(binary)], text=True)
+    assert output == "Hello, World!\n"
+
+
+def test_dotnet2_plugin_no_dotnet(new_dir, partitions, mocker):
+    """Test the dotnet plugin while pretending dotnet isn't installed."""
+
+    class FailSpecificCmdValidator(dotnet2_plugin.Dotnet2PluginEnvironmentValidator):
+        """A validator that always fails the first time we run `dotnet --version`."""
+
+        __already_run = False
+
+        @override
+        def _execute(self, cmd: str) -> str:
+            if cmd == "dotnet --version" and not self.__already_run:
+                self.__class__.__already_run = True
+                raise subprocess.CalledProcessError(127, cmd)
+            return super()._execute(cmd)
+
+    mocker.patch.object(
+        dotnet2_plugin.Dotnet2Plugin, "validator_class", FailSpecificCmdValidator
+    )
+
+    test_dotnet2_plugin(new_dir, partitions)
+
+
+def test_dotnet2_plugin_fake_dotnet(new_dir, partitions, mocker):
+    """Test the dotnet plugin while pretending dotnet is installed."""
+
+    class AlwaysFindDotnetValidator(dotnet2_plugin.Dotnet2PluginEnvironmentValidator):
+        """A validator that always succeeds the first time running `dotnet --version`."""
+
+        __already_run = False
+
+        @override
+        def _execute(self, cmd: str) -> str:
+            if cmd != "dotnet --version":
+                return super()._execute(cmd)
+            try:
+                return super()._execute(cmd)
+            except subprocess.CalledProcessError:
+                if self.__already_run:
+                    raise
+                return ""
+            finally:
+                self.__class__.__already_run = True
+
+    mocker.patch.object(
+        dotnet2_plugin.Dotnet2Plugin, "validator_class", AlwaysFindDotnetValidator
+    )
+
+    test_dotnet2_plugin(new_dir, partitions)
