02df0490d3ef3d342b3dd810ed7aead2a17b1022
Dariusz Duda <dariusz.duda@canonical.com>
2025-04-07 08:52:07 +0200

    test(integration): add cargo-registry project
    Signed-off-by: Dariusz Duda <dariusz.duda@canonical.com>

---
+++ tests/integration/plugins/test_cargo_registry/src/main.rs
@@ -0,0 +1,13 @@
+cfg_if::cfg_if! {
+    if #[cfg(unix)] {
+        fn foo() { /* unix specific functionality */ }
+    } else if #[cfg(target_pointer_width = "32")] {
+        fn foo() { /* non-unix, 32-bit functionality */ }
+    } else {
+        fn foo() { /* fallback implementation */ }
+    }
+}
+
+fn main() {
+    foo();
+}
