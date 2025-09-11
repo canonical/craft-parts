faa257acdeaa213dd5a7866e26871a80b06a7b95
Alex Lowe <alex.lowe@canonical.com>
2025-09-10 23:52:51 -0400

    fix?(RepositoryProxy): make RepositoryProxy mockable
    This makes the RepositoryProxy act more like a regular object by allowing the deletion of attributes. If I'm correct, this was the cause of the unit tests blowing up in https://github.com/canonical/snapcraft/pull/5725
    
    Signed-off-by: Alex Lowe <alex.lowe@canonical.com>

--- craft_parts/packages/__init__.py
+++ craft_parts/packages/__init__.py
@@ -67,5 +67,11 @@ class _RepositoryProxy:
         )
         setattr(repo, attr, value)
 
+    def __delattr__(self, attr: str) -> None:
+        repo = _get_repository_for_platform()
+        logger.debug(
+            "delete repository attribute: attr=%s, value=%s, repo:%s", attr, value, repo
+        )
+        delattr(repo, attr)
 
 Repository = cast("BaseRepository", _RepositoryProxy())
