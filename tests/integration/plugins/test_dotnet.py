100260a57ba6158f04d84fe3763ac5fcbb82f67f
Alex Lowe <alex.lowe@canonical.com>
2025-09-16 18:13:24 -0400

    test: limit dotnet integration tests

--- tests/integration/plugins/test_dotnet.py
+++ tests/integration/plugins/test_dotnet.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import platform
 import subprocess
 import textwrap
 from pathlib import Path
@@ -24,7 +25,12 @@ from craft_parts import LifecycleManager, Step
 from craft_parts.plugins import dotnet_plugin
 from typing_extensions import override
 
-pytestmark = [pytest.mark.plugin]
+pytestmark = [
+    pytest.mark.plugin,
+    pytest.mark.skipif(
+        platform.machine() != "amd64", reason="Dotnet v1 plugin is amd64-only."
+    ),
+]
 
 
 def test_dotnet_plugin(new_dir, partitions):
