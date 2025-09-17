e5e8279c4a215a1d7c93ca94f5e8c26e283ff616
Alex Lowe <alex.lowe@canonical.com>
2025-09-16 21:32:12 -0400

    fix(test): only install meson if necessary

--- tests/integration/plugins/test_meson.py
+++ tests/integration/plugins/test_meson.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import shutil
 import subprocess
 import textwrap
 from pathlib import Path
@@ -27,9 +28,12 @@ pytestmark = [pytest.mark.plugin]
 
 @pytest.fixture
 def meson():
-    subprocess.run(["pip", "install", "meson"], check=True)
+    has_meson = shutil.which("meson")
+    if not has_meson:
+        subprocess.run(["pip", "install", "meson"], check=True)
     yield
-    subprocess.run(["pip", "uninstall", "meson", "--yes"], check=True)
+    if not has_meson:
+        subprocess.run(["pip", "uninstall", "meson", "--yes"], check=True)
 
 
 @pytest.mark.usefixtures("mocker")
