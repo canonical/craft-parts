2fcedff877d910caa19162000c97bd85231d04ac
Alex Lowe <alex.lowe@canonical.com>
2025-09-18 22:36:23 -0400

    style(auto): format

--- tests/integration/plugins/test_gradle.py
+++ tests/integration/plugins/test_gradle.py
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import platform
 import shutil
 import subprocess
 import textwrap
