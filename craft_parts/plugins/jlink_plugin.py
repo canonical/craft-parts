7b2c04c3cbc1a1ef4bb8c7a5e188f3c353e1feed
Vladimir Petko <vladimir.petko@canonical.com>
2025-11-04 13:52:30 +0100

    fix: allow int value for jlink-multi-release

--- craft_parts/plugins/jlink_plugin.py
+++ craft_parts/plugins/jlink_plugin.py
@@ -32,7 +32,7 @@ class JLinkPluginProperties(PluginProperties, frozen=True):
     jlink_jars: list[str] = []
     jlink_extra_modules: list[str] = []
     jlink_modules: list[str] = []
-    jlink_multi_release: str = "base"
+    jlink_multi_release: int | str = "base"
 
 
 class JLinkPluginEnvironmentValidator(PluginEnvironmentValidator):
