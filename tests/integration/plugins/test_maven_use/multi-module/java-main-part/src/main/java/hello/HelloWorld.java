053b526350e4a25a0e28c73201b1d27ed0b0382b
smethnani <82812166+smethnani@users.noreply.github.com>
2026-01-20 04:43:17 +0000

    style: follow java directory convention style: formatting

--- tests/integration/plugins/test_maven_use/multi-module/java-main-part/src/main/java/hello/HelloWorld.java
+++ tests/integration/plugins/test_maven_use/multi-module/java-main-part/src/main/java/hello/HelloWorld.java
@@ -3,7 +3,7 @@ package org.starcraft.hello;
 import org.starcraft.subsubmod.Speaker;
 
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println(Speaker.hello());
-  }
+    public static void main(String[] args) {
+        System.out.println(Speaker.hello());
+    }
 }
