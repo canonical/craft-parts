b7ecf85251445439331ea649cb3b3253ed68bb3c
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2026-06-05 14:19:04 -0300

    tests: add integration test
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ tests/integration/lifecycle/test_build_environment.py
@@ -0,0 +1,54 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2026 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import textwrap
+from pathlib import Path
+
+import craft_parts
+import pytest
+import yaml
+from craft_parts import Action, ActionProperties, ActionType, Step
+
+basic_parts_yaml = textwrap.dedent(
+    """\
+    parts:
+      foo:
+        plugin: nil
+        build-environment:
+          - FOO: $FOO there  # modify the environment variable
+        override-build: |
+          echo $FOO          # print the modified content
+    """
+)
+
+
+def test_build_environment(new_dir, capfd):
+    parts = yaml.safe_load(basic_parts_yaml)
+
+    lf_kwargs = {
+        "application_name": "test_demo",
+        "cache_dir": new_dir,
+        "build_environment": ["FOO=hello"],
+    }
+
+    lf = craft_parts.LifecycleManager(parts, **lf_kwargs)
+    actions = lf.plan(Step.BUILD)
+    with lf.action_executor() as ctx:
+        ctx.execute(actions)
+
+    captured = capfd.readouterr()
+    assert captured.out == "hello there\n"
+
