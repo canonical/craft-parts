8bf459962875b287b0edb0ee57360353314ff34d
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2026-09-06 23:06:31 -0300

    chore: remove unused text fixture argument
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- tests/unit/plugins/test_qmake_plugin.py
+++ tests/unit/plugins/test_qmake_plugin.py
@@ -146,9 +146,7 @@ class TestPluginQMakePlugin:
             f"make install INSTALL_ROOT={part_info.part_install_dir}",
         ]
 
-    def test_get_build_commands_source_subdir_qmake_project_file(
-        self, setup_method_fixture, new_dir
-    ):
+    def test_get_build_commands_source_subdir_qmake_project_file(self, new_dir):
         part = Part("foo", {"source-subdir": "sources"})
         project_info = ProjectInfo(application_name="test", cache_dir=new_dir)
         project_info._parallel_build_count = 42
@@ -173,7 +171,7 @@ class TestPluginQMakePlugin:
         ]
 
     def test_get_build_commands_source_subdir_qmake_project_file_with_prefix(
-        self, setup_method_fixture, new_dir
+        self, new_dir
     ):
         part = Part("foo", {"source-subdir": "sources"})
         project_info = ProjectInfo(application_name="test", cache_dir=new_dir)
@@ -199,7 +197,7 @@ class TestPluginQMakePlugin:
         ]
 
     def test_get_build_commands_source_subdir_qmake_project_file_nested_subdir(
-        self, setup_method_fixture, new_dir
+        self, new_dir
     ):
         part = Part("foo", {"source-subdir": "sources/sub"})
         project_info = ProjectInfo(application_name="test", cache_dir=new_dir)
